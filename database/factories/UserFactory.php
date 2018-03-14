5a31025f5a42af34f33b693f709f696d8ff84c11
jgarcia <jgarcia@edutick.cl>
2018-03-14 17:10:14 -0300

    se agregan modelos,controladores y migraciones

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,11 @@
 <?php
 
-use Faker\Generator as Faker;
 
+use Faker\Generator as Faker;
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -13,11 +17,45 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    static $password;
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => $password ?: $password = bcrypt('secret'), // secret
         'remember_token' => str_random(10),
+        'verified' => $verificado = $faker->randomElement([User::USUARIO_VERIFICADO, User::USUARIO_NO_VERIFICADO]),
+        'verification_token' => $verificado == User::USUARIO_VERIFICADO ? null : User::generarVerificationToken(),
+        'admin' => $faker->randomElement([User::USUARIO_ADMINISTRADOR, User::USUARIO_REGULAR]),
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+    ];
+});
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+        //'seller_id' => User::inRandomOrder()->first()->id,
+        'seller_id' => User::all()->random()->id,
+    ];
+});
+
+$factory->define(App\Transaction::class, function (Faker\Generator $faker) {
+
+    return [
+        'name' => $faker->word,
+        'descquanription' => $faker->paragraph(1),
     ];
 });
